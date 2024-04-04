515e71c22e2d3c22beb27e577c01802baf189365
Allison Dahan <allison.dahan@yahoo.ca>
2024-04-04 13:57:09 -0700

    fully working

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,18 +1,20 @@
-const MongoClient = require("mongodb").MongoClient;
-
-const is_heroku = process.env.IS_HEROKU || false;
-
-
-const herokuURI = "mongodb://localhost/?authSource=admin&retryWrites=true&w=majority;"
-
-const localURI = "mongodb://localhost/?authSource=admin&retryWrites=true&w=majority;"
-
-if (is_heroku) {
-	var database = new MongoClient(herokuURI, {useNewUrlParser: true, useUnifiedTopology: true});
+const database = require("mongoose");
+const is_qoddi = process.env.IS_QODDI || false;
+const databaseName = "lab_example";
+const qoddiURI =
+  "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.mmhwhm3.mongodb.net/" +
+  databaseName +
+   "?retryWrites=true&w=majority&appName=Cluster0";
+const localURI =
+"mongodb://127.0.0.1/" + databaseName + "?authSource=admin&retryWrites=true&w=majority";
+if (is_qoddi) {
+  database.connect(qoddiURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+} else {
+  database.connect(localURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 }
-else {
-	var database = new MongoClient(localURI, {useNewUrlParser: true, useUnifiedTopology: true});
-}
-
-module.exports = database;
-		
